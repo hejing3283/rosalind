c2c9255c221ee4e1e384ecec648a110e02da812a
moonhj00 <violet.hj@gmail.com>
2015-12-16 09:30:54 -0500

    O(1) memory

--- rabbit_recurrence_relations.py
+++ rabbit_recurrence_relations.py
@@ -2,6 +2,7 @@
 #J.HE
 #Desp.:
 class Solution(object):
+  @profile
   def totalRabbits( self, n, k ):
     # n monthes
     # k: each pair give birth to k rabbit pairs
@@ -9,15 +10,24 @@ class Solution(object):
     if n <= 2 : 
       return 1 
     ## first try
-    # rabbit paris liveing for each month
-    dp = [ 0 for _ in range(n) ] 
-    dp[0] = 1
-    dp[1] = 1
-    for i in range(2, n) :
-      dp[i] = k * dp[i-2] + dp[i-1] 
-    return dp[-1] 
+    # # # rabbit paris liveing for each month
+    # dp = [ 0 for _ in range(n) ] 
+    # dp[0] = 1
+    # dp[1] = 1
+    # for i in range(2, n) :
+    #   dp[i] = k * dp[i-2] + dp[i-1] 
+
+    ## optimize memory 
+    dp = [1,1]
+    i = 2
+    while i < n :
+	curr =  k * dp[0] + dp[1] 
+	dp = [dp[1], curr]
+	i += 1
+    return curr 
 
 sol = Solution()
-print sol.totalRabbits(5, 3 ) 
+# print sol.totalRabbits(5, 3 ) 
+print sol.totalRabbits(35, 5 ) 
 
 
